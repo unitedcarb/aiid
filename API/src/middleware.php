9e0c1226e71d0732d9fc064c6eff49335d2d5f2f
Mika Sue <info@mikasue.com>
2018-02-08 07:12:29 -0500

    Adding aiid API

---
+++ API/src/middleware.php
@@ -0,0 +1,10 @@
+<?php
+// Application middleware
+$auth = $container->get('settings')['auth'];
+// e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    "users" => [
+        "root" =>$auth['root'],
+        "aiid" => $auth['aiid']
+    ]
+]));
