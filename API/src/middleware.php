b9e0a7ce495f86dec4a3660f4020a8832ec8e126
Mika Sue <info@mikasue.com>
2018-02-08 11:54:07 -0500

    Security in middle later  secure=>false

--- API/src/middleware.php
+++ API/src/middleware.php
@@ -3,6 +3,7 @@
 $auth = $container->get('settings')['auth'];
 // e.g: $app->add(new \Slim\Csrf\Guard);
 $app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    "secure" => false,
     "users" => [
         "root" =>$auth['root'],
         "aiid" => $auth['aiid']
