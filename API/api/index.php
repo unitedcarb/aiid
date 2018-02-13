9f17ca5f4fe897f255d87bc0bbbd37646ec6904b
Mika Sue <info@mikasue.com>
2018-02-13 12:13:30 -0500

    API enhancements

--- API/api/index.php
+++ API/api/index.php
@@ -25,7 +25,6 @@ require __DIR__ . '/../src/middleware.php';
 
 // Register routes
 require __DIR__ . '/../src/routes.php';
-require __DIR__ . '/../src/routes/getFeatures.php';
 
 // Run app
 $app->run();
--- API/src/routes.php
+++ API/src/routes.php
@@ -19,20 +19,44 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 $app->get('/features', function(Request $request, Response $response, array $args) {
     $this->logger->info("Features '/' route");
     
-    $result = getFeatures();
+    $results = getFeatures();
 
-    $results = $sth->fetchAll(PDO::FETCH_OBJ);
-    $message = array("count"=>sizeOf($results), "features"=>$results);
+    if( isset( $results["Error"]) ) {
+      $result=$response->withJson($results['Error'])->withHeader('Content-Type', 'application/json');
+    } else {
+      $message = array("count"=>sizeOf($results), "features"=>$results);
 
-    $result = $response->withJSON($message)
-    ->withHeader('Content-Type','application/json');
-   
+      $result = $response->withJSON($message)
+      ->withHeader('Content-Type','application/json');
+    }
     return $result;
   });
 
+
+  $app->get('/sidebarFeatures', function(Request $request, Response $response, array $args){
+    $message = array("message"=>"Sidebar Features!");
+
+    $result = $response->withJSON($message)
+    ->withHeader('Content-Type','application/json');
+  });
 
   $app->map(['GET','POST','PUT','DELETE','PATCH'], '/{routes:.+}', function($req, $res){
     $handler = $this->notFoundHandler;
     return $handler($req, $res);
   });
+
+
+
+  function getFeatures(){
+    try {
+      $query = "SELECT * FROM Features";
+      $sth = $this->db->prepare($query);
+      $sth->execute();
+    } catch(PDOException $e){
+      $this->logger->error("PDO Error " . $e->getMessage());
+      return array("Error"=>$e->getMessage());
+    }
+
+    $results = $sth->fetchAll(PDO::FETCH_OBJ);
+  }
   ?>
