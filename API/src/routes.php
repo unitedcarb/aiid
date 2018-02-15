61a7138321fe89f3c0468500aa827d019188264a
Mika Sue <info@mikasue.com>
2018-02-15 07:35:25 -0500

    updating sidebar

--- API/src/routes.php
+++ API/src/routes.php
@@ -3,60 +3,94 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-require "routes/getFeatures.php";
-
 // Routes
 
 $app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Root '/' route");
-
-    // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 
 $app->get('/features', function(Request $request, Response $response, array $args) {
     $this->logger->info("Features '/' route");
-    
-    $results = getFeatures();
+
+    $results = getFeatures($this->db, $this->logger);
 
     if( isset( $results["Error"]) ) {
-      $result=$response->withJson($results['Error'])->withHeader('Content-Type', 'application/json');
+      $result= $response->withJSON($results['Error'])->withHeader('Content-Type', 'application/json');
     } else {
       $message = array("count"=>sizeOf($results), "features"=>$results);
-
       $result = $response->withJSON($message)
       ->withHeader('Content-Type','application/json');
     }
     return $result;
+
   });
 
 
-  $app->get('/sidebarFeatures', function(Request $request, Response $response, array $args) {
-    $message = array("message"=>"Sidebar Features!");
+    $app->get('/sidebarFeatures', function(Request $request, Response $response, array $args) {
+      $this->logger->info("Sidebar Features '/' route");
+      $featureObj = populateSideBarFeatures($this->db, $this->logger);
 
-    $result = $response->withJSON($message)
-    ->withHeader('Content-Type','application/json');
-  });
+      $message = array("message"=> $featureObj );
+      $result = $response->withJSON($message)
+      ->withHeader('Content-Type','application/json');
 
-  $app->map(['GET','POST','PUT','DELETE','PATCH'], '/{routes:.+}', function($req, $res){
-    $handler = $this->notFoundHandler;
-    return $handler($req, $res);
-  });
+      return $result;
+
+    });
+
+    $app->map(['GET','POST','PUT','DELETE','PATCH'], '/{routes:.+}', function($req, $res){
+      $handler = $this->notFoundHandler;
+      return $handler($req, $res);
+    });
 
 
-
-  function getFeatures() {
+  function getFeatures($_db, $_logger) {
     try {
       $query = "SELECT * FROM Features";
-      $sth = $this->db->prepare($query);
+      $sth = $_db->prepare($query);
       $sth->execute();
     } catch(PDOException $e){
-      $this->logger->error("PDO Error " . $e->getMessage());
-      return array("Error"=>$e->getMessage());
+      $_logger->error("PDO Error " . $e->getMessage());
+      return array("Error"=> $e->getMessage() );
+    }
+    $results = $sth->fetchAll(PDO::FETCH_OBJ);
+    return $results;
+  }
+
+
+  function populateSideBarFeatures($_db, $_logger){
+    $featureArray = array();
+		$uniqueProducts = array();
+    $uniqueReleases = array();
+    $uniqueCategories = array();
+    $features = getFeatures($_db, $_logger);
+
+/*      foreach($f as $fetures){
+        array_push($featureArray, constructSidebarObj($f));
+      }
+  */
+      return $features;
     }
 
-    $results = $sth->fetchAll(PDO::FETCH_OBJ);
+
+  function constructSidebarObj($feature){
+    $categoryFeaturesArray = array();
+    $categoriesArray = array();
+    $releaseArray = array();
+
+    //just add feature to this category features array
+    $featureObj = array("_id"=>$feature->_id, "featureName"=>$feature->featureName);
+    array_push($categoryFeaturesArray,  $featureObj );
+    $categoryObj = array("categoryName"=>$feature->featureCategoryName, "categoryFeatures"=>$categoryFeaturesArray);
+    array_push($categoriesArray, $categoryObj);
+    $releaseObj = array("releaseName"=>$feature->releaseName, "categories"=>$categoriesArray);
+    array_push($releaseArray, $releaseObj);
+    $featureObj = array("productName"=>$feature->productName,"releases"=>$releaseArray);
+
+    return $featureObj;
   }
+
   ?>
